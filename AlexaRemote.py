1262088c54327945a0e00fc6b4f3a0d4bb308662
secretspy97 <nathan.connor@gosparx.org>
2018-04-02 19:10:14 -0400

    Added all intents for TV

--- AlexaRemote.py
+++ AlexaRemote.py
@@ -7,10 +7,39 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/remote/')
 
-# Power represents weather the TV wants to be switched on or off
 @ask.intent('TVPowerOn')
-def change_tv_power(power):
-    message = "I have been reached"
+def power_on():
+    message = "TV has been turned on"
+    return statement(message)
+
+@ask.intent('TVPowerOff')
+def power_off():
+    message = "TV has been turned off"
+    return statement(message)
+
+@ask.intent('TVVolumeUp')
+def volume_up():
+    message = "Volume has been turned up"
+    return statement(message)
+
+@ask.intent('TVVolumeDown')
+def volume_down():
+    message = "Volume has been turned down"
+    return statement(message)
+
+@ask.intent('TVVolumeSet', convert={'volume': int})
+def volume_set(volume):
+    message = render_template('volumeSet', volume=volume)
+    return statement(message)
+
+@ask.intent('TVVolumeSetup')
+def volume_setup():
+    message = "Volume has been setup"
+    return statement(message)
+
+@ask.intent('TVChangeInput')
+def input_change():
+    message = "The input has been changed"
     return statement(message)
 
 if __name__ == '__main__':
