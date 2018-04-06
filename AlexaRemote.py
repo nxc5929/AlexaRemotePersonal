5b06b941906846adad268caf43e67f8a9b1c7045
secretspy97 <nathan.connor@gosparx.org>
2018-04-06 18:07:28 -0400

    Added Lighting config

--- AlexaRemote.py
+++ AlexaRemote.py
@@ -49,17 +49,74 @@ def input_change():
     message = "The input has been changed"
     return statement(message)
 
+# LED STRIP
 
-@ask.intent('LEDLightsON')
+lightsOn = False
+
+@ask.intent('LightsOn')
 def led_lights_on():
-    message = "The lights have been turned on"
-    os.system("irsend SEND_ONCE LED KEY_POWER")
+    global lightsOn
+    if not lightsOn:
+        lightsOn = True
+        message = "The lights have been turned on"
+        os.system("irsend SEND_ONCE LED KEY_POWER")
+    else:
+        message = "Lights are already on"
     return statement(message)
 
-@ask.intent('LEDLightsOff')
+@ask.intent('LightsOff')
 def led_lights_off():
-    message = "The lights have been turned off"
-    os.system("irsend SEND_ONCE LED KEY_POWER")
+    global lightsOn
+    if lightsOn:
+        lightsOn = False
+        message = "The lights have been turned off"
+        os.system("irsend SEND_ONCE LED KEY_POWER")
+    else:
+        message = "Lights are already off"
+    return statement(message)
+
+@ask.intent('LightsBlue')
+def led_lights_blue():
+    message = "The lights have been turned blue"
+    os.system("irsend SEND_ONCE LED KEY_BLUE")
+    return statement(message)
+
+@ask.intent('LightsRed')
+def led_lights_red():
+    message = "The lights have been turned red"
+    os.system("irsend SEND_ONCE LED KEY_RED")
+    return statement(message)
+
+@ask.intent('LightsGreen')
+def led_lights_green():
+    message = "The lights have been turned green"
+    os.system("irsend SEND_ONCE LED KEY_GREEN")
+    return statement(message)
+
+@ask.intent('LightsNiceBlue')
+def led_lights_nice_blue():
+    message = "The lights have been turned a nice shade of blue"
+    os.system("irsend SEND_ONCE LED KEY_NICEBLUE")
+    return statement(message)
+
+@ask.intent('LightsFade')
+def led_lights_fade():
+    message = "The lights have been set to fade"
+    os.system("irsend SEND_ONCE LED KEY_FADE")
+    return statement(message)
+
+currentBrightness = 0
+@ask.intent('LightsBrightness', convert={'brightness': int})
+def led_lights_brightness(brightness):
+    global currentBrightness
+    message = render_template('brightnessSet', brightness=brightness)
+    if(currentBrightness < brightness):
+        for x in range(1, brightness-currentBrightness):
+            os.system("irsend SEND_ONCE LED KEY_BRIGHTUP")
+    else:
+        for x in range(1, currentBrightness-brightness):
+            os.system("irsend SEND_ONCE LED KEY_BRIGHTDOWN")
+    currentBrightness = brightness
     return statement(message)
 
 if __name__ == '__main__':
