c0464d16457095aa32cff8b172e96eb29ecfec95
secretspy97 <nathan.connor@gosparx.org>
2018-04-08 11:25:14 -0400

    Added Threading

--- AlexaRemote.py
+++ AlexaRemote.py
@@ -1,11 +1,14 @@
 import os
-import time
+from Lights import Lights, LightStates
 
 from flask import Flask, render_template
 from flask_ask import Ask, statement
 
 app = Flask(__name__)
 ask = Ask(app, '/remote/')
+
+light_thread = Lights(1, "Lights-1", 1)
+light_thread.start()
 
 @ask.launch
 def launch():
@@ -49,28 +52,21 @@ def input_change():
     message = "The input has been changed"
     return statement(message)
 
+##################################################
 # LED STRIP
+##################################################
 
-lightsOn = False
-
-@ask.intent('LightsOn')
 def led_lights_on():
-    global lightsOn
-    if not lightsOn:
-        lightsOn = True
-        message = "The lights have been turned on"
-        os.system("irsend SEND_ONCE LED KEY_POWER")
+    if light_thread.power_lights(True):
+        message = "Lights have been turned on"
     else:
         message = "Lights are already on"
     return statement(message)
 
 @ask.intent('LightsOff')
 def led_lights_off():
-    global lightsOn
-    if lightsOn:
-        lightsOn = False
+    if light_thread.power_lights(False):
         message = "The lights have been turned off"
-        os.system("irsend SEND_ONCE LED KEY_POWER")
     else:
         message = "Lights are already off"
     return statement(message)
@@ -78,51 +74,45 @@ def led_lights_off():
 @ask.intent('LightsBlue')
 def led_lights_blue():
     message = "The lights have been turned blue"
-    os.system("irsend SEND_ONCE LED KEY_BLUE")
+    light_thread.changeState(LightStates.BLUE)
     return statement(message)
 
 @ask.intent('LightsRed')
 def led_lights_red():
     message = "The lights have been turned red"
-    os.system("irsend SEND_ONCE LED KEY_RED")
+    light_thread.changeState(LightStates.RED)
     return statement(message)
 
 @ask.intent('LightsGreen')
 def led_lights_green():
     message = "The lights have been turned green"
-    os.system("irsend SEND_ONCE LED KEY_GREEN")
+    light_thread.changeState(LightStates.GREEN)
     return statement(message)
 
 @ask.intent('LightsNiceBlue')
 def led_lights_nice_blue():
     message = "The lights have been turned a nice shade of blue"
-    os.system("irsend SEND_ONCE LED KEY_NICEBLUE")
+    light_thread.changeState(LightStates.NICE_BLUE)
     return statement(message)
 
 @ask.intent('LightsWhite')
-def led_lights_nice_blue():
+def led_lights_white():
     message = "The lights have been turned white"
-    os.system("irsend SEND_ONCE LED KEY_WHITE")
+    light_thread.changeState(LightStates.WHITE)
     return statement(message)
 
 @ask.intent('LightsFade')
 def led_lights_fade():
     message = "The lights have been set to fade"
-    os.system("irsend SEND_ONCE LED KEY_FADE")
+    light_thread.changeState(LightStates.FADE)
     return statement(message)
 
-currentBrightness = 0
 @ask.intent('LightsBrightness', convert={'brightness': int})
 def led_lights_brightness(brightness):
-    global currentBrightness
-    message = render_template('brightnessSet', brightness=brightness)
-    if(currentBrightness < brightness):
-        for x in range(1, brightness-currentBrightness):
-            os.system("irsend SEND_ONCE LED KEY_BRIGHTUP")
+    if light_thread.set_brightness(brightness):
+        message = render_template('brightnessSet', brightness=brightness)
     else:
-        for x in range(1, currentBrightness-brightness):
-            os.system("irsend SEND_ONCE LED KEY_BRIGHTDOWN")
-    currentBrightness = brightness
+        message = "Brightness must be in range 0 to 8. Please try again."
     return statement(message)
 
 if __name__ == '__main__':
---
+++ Lights.py
@@ -0,0 +1,115 @@
+from enum import Enum
+import os
+from time import sleep
+import threading
+
+class LightStates(Enum):
+    STANDBY = 0
+    POWER = 1
+    BLUE = 2
+    RED = 3
+    GREEN = 4
+    NICE_BLUE = 5
+    FADE = 6
+    WHITE = 7
+    BRIGHTNESS = 8
+
+class Lights(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def reset(self):
+        global currentState
+        currentState = LightStates.STANDBY
+
+    def standby(self):
+        return
+
+    def power(self):
+        os.system("irsend SEND_ONCE LED KEY_POWER")
+        self.reset()
+
+    lightsOn = False
+    def power_lights(self, on):
+        if on != self.lightsOn:
+            self.lightsOn = on
+            self.changeState(LightStates.POWER)
+            return True
+        return False
+
+    def blue(self):
+        os.system("irsend SEND_ONCE LED KEY_BLUE")
+        self.reset()
+
+    def red(self):
+        os.system("irsend SEND_ONCE LED KEY_RED")
+        self.reset()
+
+    def green(self):
+        os.system("irsend SEND_ONCE LED KEY_GREEN")
+        self.reset()
+
+    def nice_blue(self):
+        os.system("irsend SEND_ONCE LED KEY_NICEBLUE")
+        self.reset()
+
+    def fade(self):
+        os.system("irsend SEND_ONCE LED KEY_FADE")
+        self.reset()
+
+    def white(self):
+        os.system("irsend SEND_ONCE LED KEY_WHITE")
+        self.reset()
+
+    #1-8
+    currentBrightness = 0
+    wantedBrightness = 0
+    def brightness(self):
+        if(self.currentBrightness > self.wantedBrightness):
+            while(self.currentBrightness > self.wantedBrightness):
+                os.system("irsend SEND_ONCE LED KEY_BRIGHTDOWN")
+                self.currentBrightness = self.currentBrightness - 1
+                sleep(0.5)
+        else:
+            while(self.currentBrightness < self.wantedBrightness):
+                os.system("irsend SEND_ONCE LED KEY_BRIGHTUP")
+                self.currentBrightness = self.currentBrightness + 1
+                sleep(0.5)
+        self.reset()
+
+    def set_brightness(self, bright):
+        if bright is int and bright in range(1,9):
+            self.wantedBrightness = bright
+            self.changeState(LightStates.BRIGHTNESS)
+            return True
+        else:
+            return False
+
+
+    currentState = LightStates.STANDBY
+    def changeState(self, state):
+        if state is LightStates:
+            self.currentState = state
+            print("State changed to " + str(state))
+
+    options = {
+        LightStates.STANDBY : standby,
+        LightStates.POWER : power,
+        LightStates.BLUE : blue,
+        LightStates.RED : red,
+        LightStates.GREEN : green,
+        LightStates.NICE_BLUE : nice_blue,
+        LightStates.FADE : fade,
+        LightStates.WHITE : white,
+        LightStates.BRIGHTNESS : brightness
+    }
+
+    def execute(self):
+        self.options[self.currentState](self)
+
+    def run(self):
+        while(True):
+            self.execute()
